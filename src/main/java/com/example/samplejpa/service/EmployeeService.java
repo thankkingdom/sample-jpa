10f035142c16a5a02b304392dd120288dd130d9b
thankkingdom <thanksphere@gmail.com>
2018-11-25 01:47:58 +0900

    Native Queryのサンプル追加

--- src/main/java/com/example/samplejpa/service/EmployeeService.java
+++ src/main/java/com/example/samplejpa/service/EmployeeService.java
@@ -15,7 +15,14 @@ public class EmployeeService {
 	@PersistenceContext
 	private EntityManager entityManager;
 
+	public <T> T cast(Object o) {
+		return (T) o;
+	}
+
 	public List<Employee> getEmployeeList() {
-		return entityManager.createQuery("SELECT e FROM Employee e", Employee.class).getResultList();
+		List<Employee> list = entityManager.createQuery("SELECT e FROM Employee e", Employee.class).getResultList();
+		list = entityManager.createNativeQuery("SELECT * FROM jpa.employee", Employee.class).getResultList();
+		list = cast(entityManager.createNativeQuery("SELECT * FROM jpa.employee", Employee.class).getResultList());
+		return list;
 	}
 }

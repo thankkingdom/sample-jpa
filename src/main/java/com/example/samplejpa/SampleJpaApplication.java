2ecd048be9fa7ca1913d1fb8994ea34587ab63bf
thankkingdom <thanksphere@gmail.com>
2018-11-24 13:14:08 +0900

    初期構築

---
+++ src/main/java/com/example/samplejpa/SampleJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example.samplejpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJpaApplication.class, args);
+	}
+}
